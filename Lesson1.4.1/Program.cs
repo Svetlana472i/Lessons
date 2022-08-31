b56cabd4b3569fcc4759aa9c041f3f9f31ad43ae
Svetlana472i <tito472i@mail.ru>
2022-08-31 23:52:26 +0400

    add number in place three vers2

---
+++ Lesson1.4.1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int number1 = number % 100;
+int number2 = number % 10;
+int number3 = number/100;
+
+if (number3>9)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+   Console.WriteLine(number2); 
+}
