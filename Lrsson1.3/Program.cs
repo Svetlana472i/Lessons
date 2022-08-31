d6f63fcfc6441d7ac5cd52e9f5d9a88b29beff0a
Svetlana472i <tito472i@mail.ru>
2022-08-31 22:10:57 +0400

    add number from -N to N

---
+++ Lrsson1.3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+int current = -1 * number;
+while (current <= number)
+{
+    Console.Write(current);
+    Console.Write(", ");
+    current = current+1;
+}
