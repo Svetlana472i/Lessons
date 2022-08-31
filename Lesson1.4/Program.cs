ea57859420c664407956a175c9c162b14bfe9e59
Svetlana472i <tito472i@mail.ru>
2022-08-31 23:23:46 +0400

    add Number place three

---
+++ Lesson1.4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result1 = number/100;
+int result2 = number - result1 * 100;
+int result3 = result2/10;
+int result = number - result1 *100 - result3 *10;
+Console.WriteLine(result);
