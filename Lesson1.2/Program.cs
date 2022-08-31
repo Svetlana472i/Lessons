97d8ff66e156c4c0fd72b05a6c60d52499e290b5
Svetlana472i <tito472i@mail.ru>
2022-08-31 16:00:20 +0400

    add Number Week

---
+++ Lesson1.2/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int numberWeek = Convert.ToInt32(Console.ReadLine());
+
+if (numberWeek == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+
+if (numberWeek == 2)
+{
+    Console.WriteLine("Вторник");
+}
+
+if (numberWeek == 3)
+{
+    Console.WriteLine("Среда");
+}
+
+if (numberWeek == 4)
+{
+    Console.WriteLine("Четверг");
+}
+
+if (numberWeek == 5)
+{
+    Console.WriteLine("Пятница");
+}
+
+if (numberWeek == 6)
+{
+    Console.WriteLine("Суббота");
+}
+
+if (numberWeek == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+
+if  (numberWeek != 1 && numberWeek != 2 && numberWeek != 3 && numberWeek != 4 && numberWeek != 5 && numberWeek != 6 && numberWeek != 7)
+{
+    Console.WriteLine("В неделе всего семь дней))");
+}
