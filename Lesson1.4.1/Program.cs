edaed71b5c46bebc912b63579f986479ff10e818
Svetlana472i <tito472i@mail.ru>
2022-09-02 16:02:26 +0400

    add number>999, or<100

--- Lesson1.4.1/Program.cs
+++ Lesson1.4.1/Program.cs
@@ -5,7 +5,7 @@ int number1 = number % 100;
 int number2 = number % 10;
 int number3 = number/100;
 
-if (number3>9)
+if (number3>9 | number3< 1)
 {
     Console.WriteLine("Вы ввели не трехзначное число");
 }
