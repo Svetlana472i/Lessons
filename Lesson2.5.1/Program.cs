007f8383aef261b88c5409733f561535721c0deb
Svetlana <tito472i@mail.ru>
2022-09-04 21:53:59 +0300

    add Lesson2.5.1

---
+++ Lesson2.5.1/Program.cs
@@ -0,0 +1,22 @@
+﻿bool CheckNumber(int number, int firstDivide, int secondDivide)
+{
+    if (number % firstDivide == 0 && number % secondDivide == 0)
+{
+    return true;
+}
+    else
+{
+    return false;
+}
+}
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите первый делитель");
+int firstDivide = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второй делитель");
+int secondDivide = Convert.ToInt32(Console.ReadLine());
+
+bool result = CheckNumber(number, firstDivide, secondDivide);
+    Console.WriteLine(result);
