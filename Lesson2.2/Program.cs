d4828ed60403f4e11769786bc054bad338401409
Svetlana <tito472i@mail.ru>
2022-09-04 14:12:35 +0300

    Lesson 2.2

---
+++ Lesson2.2/Program.cs
@@ -0,0 +1,11 @@
+﻿Random random = new Random();
+int randomNumber = random.Next(100,1000);
+
+Console.WriteLine(randomNumber);
+
+int firstNumber = randomNumber / 100;
+int number = randomNumber % 100;
+int lastNumber = number % 10;
+int result = 10 * firstNumber + lastNumber;
+
+Console.WriteLine(result);
