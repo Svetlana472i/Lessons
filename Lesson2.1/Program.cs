19dc5590f8b80e17ab9746769aeebd8e85d97224
Svetlana <tito472i@mail.ru>
2022-09-04 13:57:04 +0300

    Lesson2.1

---
+++ Lesson2.1/Program.cs
@@ -0,0 +1,19 @@
+﻿Random random = new Random();
+int randomNumber = random.Next(10,100);
+
+Console.WriteLine(randomNumber);
+
+int firstNumber = randomNumber / 10;
+int lastNumber = randomNumber % 10;
+
+Console.WriteLine(firstNumber);
+Console.WriteLine(lastNumber);
+
+if (lastNumber > firstNumber)
+{
+    Console.WriteLine($"Максимальное число равно {lastNumber}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число равно {firstNumber}");
+}
