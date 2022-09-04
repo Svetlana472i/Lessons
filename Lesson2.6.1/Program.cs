b1d519a945abae315269b218d8fe59d71be5e6c8
Svetlana <tito472i@mail.ru>
2022-09-04 22:04:18 +0300

    add lesson2.6.1

---
+++ Lesson2.6.1/Program.cs
@@ -0,0 +1,19 @@
+﻿bool checkSqrt(int number1, int number2)
+{
+if (number1 * number1 == number2 | number2 * number2 == number1)
+{
+   return true;
+}
+else
+{
+   return false;
+}
+}
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = checkSqrt(number1, number2);
+
+Console.WriteLine(result);
