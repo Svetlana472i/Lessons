3e462f407ba7f7d56b49016e4a48387ec1e02535
Svetlana <tito472i@mail.ru>
2022-09-04 14:40:27 +0300

    Lesson3.2

---
+++ Lesson3.2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = (number2 % number1);
+if (result == 0)
+{
+  Console.WriteLine("Второе число кратно первому");  
+}
+else
+{
+    Console.WriteLine($"Остаток от деления равен {result}");
+}
