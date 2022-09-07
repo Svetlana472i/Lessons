075a9ade83592751a278f056c69779cf08e8287f
Svetlana <tito472i@mail.ru>
2022-09-07 19:10:58 +0300

    add random array

---
+++ Lesson4.3/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = new int[8];
+void FillArray(int[] bin)
+{
+    int length = bin.Length;
+    for (int i = 0; i < length; i++)
+    {
+        bin[i] = new Random().Next(0, 2);
+    }
+   
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+FillArray(array);
+PrintArray(array);
