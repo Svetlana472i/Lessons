ac48156a0c134d1a37c574c5a51fd81e5f8aaac8
Svetlana <tito472i@mail.ru>
2022-09-07 18:26:58 +0300

    add summ number

---
+++ Lesson4.1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Input number");
+bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+
+if (isNumber == false)
+{
+    Console.WriteLine("Is not valid");
+    return;
+}
+
+int GetSumNumber(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+      sum = sum + i;    
+    }
+    return sum;
+}
+
+int result = GetSumNumber(number);
+
+Console.WriteLine(result);
+
