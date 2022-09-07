886e65d12c139971353bf627ea9cefb2d166a680
Svetlana <tito472i@mail.ru>
2022-09-07 18:45:03 +0300

    add composition number

---
+++ Lesson4.2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Input number");
+bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+
+if (isNumber == false)
+{
+    Console.WriteLine("Is not valid");
+    return;
+}
+
+int GetComposition(int n)
+{
+    int composition = 1;
+    for (int i = 2; i <=n; i++)
+   {
+    composition = composition * i;
+   }
+   return composition;
+}
+
+int result = GetComposition(number);
+Console.WriteLine(result);
