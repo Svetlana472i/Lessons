32149ce58aa8d1457c0623538adf29a204de67a0
Svetlana <tito472i@mail.ru>
2022-09-07 19:23:09 +0300

    add length number

---
+++ Lesson4.4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Input number");
+bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+
+if (isNumber == false)
+{
+    Console.WriteLine("Is not valid");
+    return;
+}
+
+int GetLengthNumber(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        count++;
+        number = number / 10;
+    }
+    return count;
+}
+int result = GetLengthNumber(number);
+Console.WriteLine(result);
