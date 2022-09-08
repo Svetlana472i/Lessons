b1b625b5117f149db2a011df48851f8f9c80c277
Svetlana <tito472i@mail.ru>
2022-09-08 14:07:43 +0300

    ChengeNumberInArray

---
+++ Lesson5.2/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Введите длину массива");
+bool isNumber = int.TryParse(Console.ReadLine(), out int length);
+
+if (isNumber == false || length<=0)
+{
+    Console.WriteLine("Длина введена неверно");
+    return;
+}
+
+int[] FillArray(int Length)
+{
+    int[] array = new int[length];
+    string input = Console.ReadLine();
+    string[] inputArray = input.Split(",");
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = Convert.ToInt32(inputArray[i]);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+Console.Write("[");
+for (int i = 0; i < array.Length-1; i++)
+{
+    Console.Write($"{array[i]}, ");
+}
+Console.Write(array[array.Length-1]);
+Console.WriteLine("]");
+}
+
+int[] ChengeNumberInArray(int[] arr)
+{
+    for (int i = 0; i < length; i++)
+    {
+       arr[i] = -arr[i];
+    }
+    return arr;
+}
+
+int[] array = FillArray(length);
+PrintArray(array);
+int[] arr = ChengeNumberInArray(array);
+PrintArray(arr);
