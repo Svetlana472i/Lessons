e4f9f121773089c5cc5970b809138a0dea030486
Svetlana <tito472i@mail.ru>
2022-09-08 21:12:49 +0300

    Fibonacci

---
+++ Lesson6.3/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число");
+bool isNumber = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isNumber || n <=0)
+{
+    Console.WriteLine("Данные введены неверно");
+    return;
+}
+int[] GetFibonacci(int n)
+{   
+    int[] result = new int[n];
+    result[0] = 0;
+    result[1] = 1;
+    for (int i = 2; i < n; i++)
+    {
+        result[i] = result[i-1] + result[i-2];
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+}
+
+int[] Fibonacci = GetFibonacci(n);
+PrintArray(Fibonacci);
