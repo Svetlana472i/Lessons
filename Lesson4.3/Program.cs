40d7daef9064e2b2047ea302fa17803b75c95ff0
Svetlana <tito472i@mail.ru>
2022-09-08 12:23:33 +0300

    add array

--- Lesson4.3/Program.cs
+++ Lesson4.3/Program.cs
@@ -1,7 +1,15 @@
-﻿int[] array = new int[8];
+﻿Console.WriteLine("Input length array");
+bool isNumber = int.TryParse(Console.ReadLine(), out int n);
+
+if (isNumber == false || n<=0)
+{
+    Console.WriteLine("Is not valid");
+    return;
+}
+int[] array = new int[n];
 void FillArray(int[] bin)
 {
-    int length = bin.Length;
+     int length = bin.Length;
     for (int i = 0; i < length; i++)
     {
         bin[i] = new Random().Next(0, 2);
