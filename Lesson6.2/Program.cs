0e61a8b432ea0eff1532b1aad39bb8487ab4fb98
Svetlana <tito472i@mail.ru>
2022-09-08 20:15:15 +0300

    To double

---
+++ Lesson6.2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число");
+bool isNumber = int.TryParse(Console.ReadLine(), out int x);
+
+if (!isNumber || x <=0)
+{
+    Console.WriteLine("Данные введены неверно");
+    return;
+}
+
+string number = ConvertNumber(x);
+string ConvertNumber(int x)
+{
+    string result = "";
+    int y;
+    while (x > 0)
+    {
+        y = x % 2;
+        x = x / 2;
+        result = Convert.ToInt32(y) + result;
+    }
+    return result;
+}
+Console.WriteLine(number);
