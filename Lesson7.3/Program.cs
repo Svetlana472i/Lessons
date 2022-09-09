d21d21da0238b4996014b52f30146bb3e5b9e8f9
Svetlana <tito472i@mail.ru>
2022-09-09 20:58:39 +0300

    sun diagonale

---
+++ Lesson7.3/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.WriteLine("Введите количество строк");
+bool isNumberm = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Введите количество столбцов");
+bool isNumbern = int.TryParse(Console.ReadLine(), out int n);
+
+
+if (!isNumberm || m <= 0 || !isNumbern || n <= 0)
+{
+    Console.WriteLine("Данные введены неверно");
+    return;
+}
+
+int[,] FillArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = random.Next(0,100);
+        }
+    }
+
+    return array;
+}
+int  sumDiagonale = 0;
+int GetSumDiagonale(int [,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        if (i == j )
+        { 
+            sumDiagonale = sumDiagonale + array[i, j];
+        }
+    }
+
+    return sumDiagonale;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] result = FillArray(m,n);
+Print2DArray(result);
+int sum = GetSumDiagonale(result);
+Console.WriteLine("сумма элементов главной диагонали равна " + sumDiagonale);
+
